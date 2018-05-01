e8f9476fb5a71ac4748f987f7e68b6ada0c73e34
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-01 22:32:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,42 @@
 var chocolateBars = ["snickers", "hundred grand","kitkat","skittles"]
 
-function addElementToBeginningOfArray(e, arr){
+function addElementToBeginningOfArray(arr, e){
   return [e, ...arr]
 }
 
-function destructivelyAddElementToBeginningOfArray(e, arr){
-  return arr.push(e)
+function destructivelyAddElementToBeginningOfArray(arr, e){
+  arr.unshift(e)
+  return arr
+}
+
+function addElementToEndOfArray(arr, e){
+  return [...arr, e]
+}
+
+function destructivelyAddElementToEndOfArray(arr, e){
+  arr.push(e)
+  return arr
+}
+
+function accessElementInArray(arr, index){
+  return arr[index]
+}
+
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift()
+  return arr
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length - 1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop()
+  return arr
 }
